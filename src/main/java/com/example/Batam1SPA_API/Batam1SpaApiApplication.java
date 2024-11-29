813d1420395e5ac6dfa9171e6b785cedea2d6708
Chelle007 <mc.chellechan@gmail.com>
2024-11-29 14:33:21 +0800

    Initial commit

---
+++ src/main/java/com/example/Batam1SPA_API/Batam1SpaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Batam1SPA_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Batam1SpaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Batam1SpaApiApplication.class, args);
+	}
+
+}
