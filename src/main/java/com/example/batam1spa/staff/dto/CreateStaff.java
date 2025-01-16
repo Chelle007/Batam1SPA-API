1f5cb1ccd6c130ae8029fc02ec5683f32ee04ccf
desraymondz <desraymondz@gmail.com>
2025-01-16 15:04:59 +0800

    add DTO entity conversion

--- src/main/java/com/example/batam1spa/staff/dto/CreateStaff.java
+++ src/main/java/com/example/batam1spa/staff/dto/CreateStaff.java
@@ -1,103 +1,23 @@
-package com.example.batam1spa.staff.model;
+package com.example.batam1spa.staff.dto;
 
 import com.example.batam1spa.availability.model.Time;
-import com.example.batam1spa.common.model.Auditable;
 import com.example.batam1spa.common.model.Gender;
 import com.example.batam1spa.service.model.ServiceType;
-import jakarta.persistence.*;
-import lombok.*;
 
-@Getter
-@Setter
-@Builder
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Table(name = "tbl_staffs")
-public class Staff extends Auditable {
-//    TODO
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(
-//            name = "shift_id",
-//            nullable = false,
-//            referencedColumnName = "id"
-//    )
-//    private Shift shift
-
-    @Column(nullable = false)
+public class CreateStaff {
     private String fullName;
-
-    @Column(nullable = false)
     private Gender gender;
-
-    @Column(nullable = false)
     private ServiceType serviceType;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(
-            name = "time_id",
-            nullable = false,
-            referencedColumnName = "id"
-    )
     private Time startTime;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(
-            name = "time_id",
-            nullable = false,
-            referencedColumnName = "id"
-    )
     private Time endTime;
-
-    @Column(
-            nullable = false,
-            columnDefinition = "boolean default false"
-    )
     private boolean isMonday;
-
-    @Column(
-            nullable = false,
-            columnDefinition = "boolean default false"
-    )
     private boolean isTuesday;
-
-    @Column(
-            nullable = false,
-            columnDefinition = "boolean default false"
-    )
     private boolean isWednesday;
-
-    @Column(
-            nullable = false,
-            columnDefinition = "boolean default false"
-    )
     private boolean isThursday;
-
-    @Column(
-            nullable = false,
-            columnDefinition = "boolean default false"
-    )
     private boolean isFriday;
-
-    @Column(
-            nullable = false,
-            columnDefinition = "boolean default false"
-    )
     private boolean isSaturday;
-
-    @Column(
-            nullable = false,
-            columnDefinition = "boolean default false"
-    )
     private boolean isSunday;
-
-    @Column(
-            nullable = false,
-            columnDefinition = "boolean default true"
-    )
     private boolean isActive;
-
-    // Getters and Setters
 
     public String getFullName() {
         return fullName;
--- src/main/java/com/example/batam1spa/staff/service/StaffServiceImpl.java
+++ src/main/java/com/example/batam1spa/staff/service/StaffServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.batam1spa.staff.service;
 
+import com.example.batam1spa.staff.dto.CreateStaff;
 import com.example.batam1spa.staff.model.Staff;
 import com.example.batam1spa.staff.repository.StaffRepository;
 import lombok.RequiredArgsConstructor;
@@ -35,4 +36,42 @@ public class StaffServiceImpl implements StaffService {
         return staffRepository.save(staff);
     }
 
+    // Convert Entity to DTO
+    private CreateStaff convertToDTO(Staff staff) {
+        CreateStaff createStaffDTO = new CreateStaff();
+        createStaffDTO.setFullName(staff.getFullName());
+        createStaffDTO.setGender(staff.getGender());
+        createStaffDTO.setServiceType(staff.getServiceType());
+        createStaffDTO.setStartTime(staff.getStartTime());
+        createStaffDTO.setEndTime(staff.getEndTime());
+        createStaffDTO.setMonday(staff.isMonday());
+        createStaffDTO.setTuesday(staff.isTuesday());
+        createStaffDTO.setWednesday(staff.isWednesday());
+        createStaffDTO.setThursday(staff.isThursday());
+        createStaffDTO.setFriday(staff.isFriday());
+        createStaffDTO.setSaturday(staff.isSaturday());
+        createStaffDTO.setSunday(staff.isSunday());
+        createStaffDTO.setActive(staff.isActive());
+        return createStaffDTO;
+    }
+
+    // Convert DTO to Entity
+    // TODO: use this when converting the parameter input from FE
+    private Staff convertToEntity(CreateStaff createStaffDTO) {
+        Staff staff = new Staff();
+        staff.setFullName(createStaffDTO.getFullName());
+        staff.setGender(createStaffDTO.getGender());
+        staff.setServiceType(createStaffDTO.getServiceType());
+        staff.setStartTime(createStaffDTO.getStartTime());
+        staff.setEndTime(createStaffDTO.getEndTime());
+        staff.setMonday(createStaffDTO.isMonday());
+        staff.setTuesday(createStaffDTO.isTuesday());
+        staff.setWednesday(createStaffDTO.isWednesday());
+        staff.setThursday(createStaffDTO.isThursday());
+        staff.setFriday(createStaffDTO.isFriday());
+        staff.setSaturday(createStaffDTO.isSaturday());
+        staff.setSunday(createStaffDTO.isSunday());
+        staff.setActive(createStaffDTO.isActive());
+        return staff;
+    }
 }
