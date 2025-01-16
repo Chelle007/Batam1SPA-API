6349a97bc8a53e63779244579edd3bec1c65f563
desraymondz <desraymondz@gmail.com>
2025-01-16 14:11:35 +0800

    add getAllStaff and addStaff
    TODO:
    use DTO

---
+++ src/main/java/com/example/batam1spa/staff/controller/StaffController.java
@@ -0,0 +1,28 @@
+package com.example.batam1spa.staff.controller;
+
+import com.example.batam1spa.staff.model.Staff;
+import com.example.batam1spa.staff.service.StaffService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/staff")
+public class StaffController {
+
+    @Autowired
+    private StaffService staffService;
+
+    // Get all staff members Full URI: /api/staff/get-all-staff
+    @GetMapping("/get-all-staff")
+    public List<Staff> getAllStaff() {
+        return staffService.getAllStaff();
+    }
+
+    // Add a new staff member Full URI: /api/staff/add
+    @PostMapping("/add")
+    public Staff addStaff(@RequestBody Staff staff) {
+        return staffService.addStaff(staff);
+    }
+}
--- src/main/java/com/example/batam1spa/staff/service/StaffServiceImpl.java
+++ src/main/java/com/example/batam1spa/staff/service/StaffServiceImpl.java
@@ -1,14 +1,19 @@
 package com.example.batam1spa.staff.service;
 
+import com.example.batam1spa.staff.model.Staff;
 import com.example.batam1spa.staff.repository.StaffRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class StaffServiceImpl implements StaffService {
+//    @Autowired
     private final StaffRepository staffRepository;
 
     @Override
@@ -19,4 +24,15 @@ public class StaffServiceImpl implements StaffService {
     private void createStaffIfNotExists() {
 
     }
+
+    // Get all staff members
+    public List<Staff> getAllStaff() {
+        return staffRepository.findAll();
+    }
+
+    // Add a new staff member
+    public Staff addStaff(Staff staff) {
+        return staffRepository.save(staff);
+    }
+
 }
