66ead1adc5bd66a730e6fef337d7f85b3bb1967a
desraymondz <desraymondz@gmail.com>
2025-02-10 21:21:11 +0800

    add addService controller

--- src/main/java/com/example/batam1spa/service/controller/ServiceController.java
+++ src/main/java/com/example/batam1spa/service/controller/ServiceController.java
@@ -1,16 +1,16 @@
 package com.example.batam1spa.service.controller;
 
 import com.example.batam1spa.common.dto.BaseResponse;
+import com.example.batam1spa.service.dto.CreateServiceRequest;
 import com.example.batam1spa.service.dto.ServiceRequest;
 import com.example.batam1spa.service.model.ServiceDescription;
+import com.example.batam1spa.service.model.Service;
 import com.example.batam1spa.service.service.ServiceDescriptionService;
 import com.example.batam1spa.service.service.ServiceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,7 +21,7 @@ public class ServiceController {
     private final ServiceService serviceService;
     private final ServiceDescriptionService serviceDescription;
 
-    // Get all service members Full URI: /api/service/get-all-service
+    // Get all service members Full URI: /api/v1/service/get-all-service
     @GetMapping("/get-all-service")
     public ResponseEntity<BaseResponse<List<ServiceRequest>>> getAllServices() {
         List<ServiceRequest> services = serviceService.getAllService();
@@ -33,7 +33,7 @@ public class ServiceController {
         return ResponseEntity.ok(response);
     }
 
-    // Get all service members Full URI: /api/service/description
+    // Get all service members Full URI: /api/v1/service/description
     @GetMapping("/description")
     public ResponseEntity<BaseResponse<List<ServiceDescription>>> getAllServicesDescription() {
         List<ServiceDescription> serviceDescriptions = serviceDescription.getAllServiceDescriptions();
@@ -45,4 +45,15 @@ public class ServiceController {
         return ResponseEntity.ok(response);
     }
 
+    // Full URL: `POST /api/v1/service/add`
+    @PostMapping("/add")
+    public ResponseEntity<BaseResponse<Service>> addService(@RequestBody CreateServiceRequest createServiceRequest) {
+        Service createdService = serviceService.addService(createServiceRequest);
+
+        // Wrap the response in BaseResponse
+        BaseResponse<Service> response = BaseResponse.success(
+                HttpStatus.CREATED, createdService, "New service created successfully");
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
